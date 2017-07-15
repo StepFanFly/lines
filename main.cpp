e6e2fae232a3943fb6f551ebd1d8276c12e7876e
Step <zsm91@mail.ru>
2017-07-15 11:44:06 +0300

    fixed "qwindows" plugin problen

--- main.cpp
+++ main.cpp
@@ -3,9 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    QStringList paths = QCoreApplication::libraryPaths();
+    paths.append(".");
+    paths.append("platforms");
+    QCoreApplication::setLibraryPaths(paths);
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
+
 
     return a.exec();
 }
